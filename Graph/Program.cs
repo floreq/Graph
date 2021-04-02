fac2196ce5b9ff2e319006d87848ebf6bd1165b5
Piotr Florczak <piotrflorczakk@gmail.com>
2021-04-02 18:58:22 +0200

    Wykonanie mutacji dla drzew z pliku tree-log.csv

--- Graph/Program.cs
+++ Graph/Program.cs
@@ -23,20 +23,73 @@ namespace Graph
             v.Add(new Vertice("10"));
             var wm = new WeightMatrix(v);
 
-            using StreamWriter file = new("../../../../tree-log.csv"); // Utworzenie pliku
-            file.WriteLine("Index;Weight;Edges;"); // Dodanie naglowkow
-            for (int i = 0; i < 100; i++)
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    var tree = new Tree();
+            //    tree.Generate(v);
+            //    tree.SetEdgesWeight(wm);
+
+            //    Console.WriteLine("Orginal: {0}", tree);
+
+            //    tree.Mutation();
+            //    Console.WriteLine("Mutation: {0}", tree);
+            //}
+
+            // Utworzenie pliku
+            //using (StreamWriter file = new("../../../../tree-log.csv"))
+            //{
+            //    file.WriteLine("Index;Weight;Edges;"); // Dodanie naglowkow
+            //    for (int i = 0; i < 100; i++)
+            //    {
+            //        var tree = new Tree();
+            //        tree.Generate(v);
+            //        tree.SetEdgesWeight(wm);
+
+            //        var sb = new StringBuilder();
+            //        sb.Append($"{i};");
+            //        sb.Append(tree);
+
+            //        file.WriteLine(sb); // Dodanie drzewa do pliku
+            //    }
+            //}
+
+            //Odczytanie z pliku
+            List<Tree> trees = new List<Tree>();
+            using (StreamReader file = new StreamReader("../../../../tree-log.csv"))
             {
-                var tree = new Tree();
-                tree.Generate(v);
-                tree.SetEdgesWeight(wm);
+                file.ReadLine();
+                while (!file.EndOfStream)
+                {
+                    var line = file.ReadLine().Split(';');
+                    var e = new List<Edge>();
 
-                var sb = new StringBuilder();
-                sb.Append($"{i};");
-                sb.Append(tree);
-                Console.WriteLine(sb);
+                    for (int i = 2; i < line.Length - 1; i++)
+                    {
+                        var column = line[i].Split(',');
+                        var edge = new Edge(new Vertice(column[0]), new Vertice(column[1]));
+                        edge.Weight = wm.GetEdgeWeight(edge);
 
-                file.WriteLine(sb); // Dodanie drzewa do pliku
+                        e.Add(edge);
+                    }
+
+                    trees.Add(new Tree(e));
+                }
+            }
+
+            // Utworzenie pliku
+            using (StreamWriter file = new("../../../../tree-mutations-log.csv"))
+            {
+                file.WriteLine("Index;Weight;Edges;"); // Dodanie naglowkow
+                for (int i = 0; i < 100; i++)
+                {
+                    trees[i].Mutation();
+                    trees[i].SetEdgesWeight(wm);
+                    var sb = new StringBuilder();
+                    sb.Append($"{i};");
+                    sb.Append(trees[i]);
+
+                    file.WriteLine(sb); // Dodanie drzewa do pliku
+                }
             }
         }
     }
